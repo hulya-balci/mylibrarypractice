ac2709bbcf4b3e8479f0174ff383b40c46715414
Hilal Ay <hhilalayy1@gmail.com>
2020-11-20 14:28:59 +0100

    recordsfeature

---
+++ src/test/java/com/library/step_definitions/BookRecordsStepDefs.java
@@ -0,0 +1,28 @@
+package com.library.step_definitions;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class BookRecordsStepDefs {
+
+
+    @When("go to the books page")
+    public void go_to_the_books_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the default record shows {int}")
+    public void the_default_record_shows(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("records options have {int} numbers")
+    public void records_options_have_numbers(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+}
